73b4c16599aab191c9000fc8c8e809e426d79650
kushitranslations <kushitranslations@gmail.com>
2021-02-14 05:43:33 +0000

    app specific pass

--- translation_project/settings.py
+++ translation_project/settings.py
@@ -137,4 +137,4 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'kushitranslations@gmail.com'
-EMAIL_HOST_PASSWORD = 'MCCHAGLAisawesome'
+EMAIL_HOST_PASSWORD = 'ydcjmrcjicouwdlo'
